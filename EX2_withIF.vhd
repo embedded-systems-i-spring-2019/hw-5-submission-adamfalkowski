e314ca528d978bf985f17d6ee19b8a82c65ad136
Adam Falkowski <47360465+adamfalkowski@users.noreply.github.com>
2019-03-06 22:40:33 -0500

    Update EX2_withIF.vhd

--- EX2_withIF.vhd
+++ EX2_withIF.vhd
@@ -8,6 +8,23 @@ entity crk2 is
        );
  end crk2;
  
- architecture if_arc of crk2 
+architecture if_arc of crk2 
  
+signal vec_input : std_logic_vector(4 downto 0);
+  
+ begin 
+ vec_input <=  A_1&A_2&B_1&B_2&D_1;
+   process
+    begin 
+        
+            --output e_out only zero when all for input into the OR are zero
+          if(vec_input = "00001") then E_out <= '0';
+          else if (vec_input = "10001") then E_out <= '0';
+          else if (vec_input ="01001" ) then E_out <= '0';
+          else if (vec_input = "00000") then E_out <= '0';
+          else 
+		  E_out <='1';
+        end case;
+      end process;
+end if_arc;
  
