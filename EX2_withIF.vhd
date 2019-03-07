4e74fe48bd6c96686536ac2f1159cd8aa798040e
Adam Falkowski <47360465+adamfalkowski@users.noreply.github.com>
2019-03-06 22:36:49 -0500

    Create EX2_withIF.vhd

---
+++ EX2_withIF.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity crk2 is
+  port (
+        A_1, A_2, B_1, B_2, D_1 : in std_logic;
+	      E_out : out std_logic
+       );
+ end crk2;
+ 
+ architecture if_arc of crk2 
+ 
+ 
